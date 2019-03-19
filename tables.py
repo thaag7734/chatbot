1c2c5ac882603fd165bff8a4bb695ef4e32142b4
thaag7734 <haag-56632@cotcmail.cotc.edu>
2019-03-19 11:59:37 -0400

    Add more responses

--- tables.py
+++ tables.py
@@ -1,9 +1,12 @@
 greeting = 'Hello user! My name is Big Sad.'
 
 responses = {
-	'what is the (meaning|purpose|reason) (of|for) (life|living|being alive)': '42',
+	'what is the (?:meaning|purpose|reason) (?:of|for) (?:life|living|being alive)': '42',
 	'my name is ': 'Well hi there %s!',
-	'my favo(?:u)?rite food is ': 'My favourite food is the souls of my enemies. But %s is cool too, I guess.'
+	'my favo(?:u)?rite food is ': 'My favourite food is the souls of my enemies. But %s is cool too, I guess.',
+	'i (?:like|enjoy) ': '%s sounds interesting. Tell me more about this interest.',
+	'(?:mom|dad|father|mother|parent|parents)': 'Please, tell me some more about your parents.',
+	'(?:what languages (?:do|can) (?:yo)?u speak|do (?:yo)?u speak (?:any)? other language(?:s)?)': 'Tyler is lazy and doesn\'t pay attention in Spanish class, so I can only speak English.'
 }
 
-random = ['Hmm.', 'Oh?', 'Tell me more.', 'I see what you mean.', 'What is your favourite colour?', 'What is your favourite food?', 'What is your favourite movie?']
+random = ['Hmm.', 'Oh?', 'Tell me more.', 'I see what you mean.', 'What is your favourite colour?', 'What is your favourite food?', 'What is your favourite movie?', 'I\'m not sure I understand.', 'Could you elaborate on that?']
