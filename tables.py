163754a48c5d57e079c0dba3ea035ea4ac052b93
thaag7734 <haag-56632@cotcmail.cotc.edu>
2019-03-20 08:17:28 -0400

    Add CCR easter egg

--- tables.py
+++ tables.py
@@ -7,7 +7,8 @@ responses = {
 	'i (?:like|enjoy) (.+)': '%s sounds interesting. Tell me more about this interest.',
 	'(?:mom|dad|father|mother|parent|parents)': 'Please, tell me some more about your parents.',
 	'(?:what languages (?:do|can) (?:yo)?u speak|(?:do|can) (?:yo)?u speak(?: any)? other language(?:s)?)': 'Tyler is lazy and doesn\'t pay attention in Spanish class, so I can only speak English.',
-	'how (?:do|can) (?:i|you) (.+)': 'I\'m opening a WikiHow article about how to %s for you.'
+	'how (?:do|can) (?:i|you) (.+)': 'I\'m opening a WikiHow article about how to %s for you.',
+	'(?:ccr|creedence|clearwater)': 'This easter egg is pretty unfortunate, son.'
 }
 
 random = ['Hmm.', 'Oh?', 'Tell me more.', 'I see what you mean.', 'What is your favourite colour?', 'What is your favourite food?', 'What is your favourite movie?', 'I\'m not sure I understand.', 'Could you elaborate on that?']
