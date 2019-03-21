af58894e6efbff0896e0733b3868a7bf81177b7d
thaag7734 <haag-56632@cotcmail.cotc.edu>
2019-03-21 08:28:33 -0400

    Add a lot of responses

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 
 engine = pyttsx3.init()
 
-favouriteFood = 'I don\'t know your favourite food.'
-favouriteMovie = 'I don\'t know your favourite movie.'
+favouriteFood 	= 'I don\'t know your favourite food.'
+favouriteMovie 	= 'I don\'t know your favourite movie.'
 favouriteColour = 'I don\'t know your favourite colour.'
 
 def respond(userSaid):
@@ -17,13 +17,13 @@ def respond(userSaid):
 	global favouriteColour
 	matched = False
 
-	whatfood = re.search('(what is|what(?:\')?s) my favo(?:u)?rite food', userSaid, re.IGNORECASE)
-	foodmatch = re.search('my favo(?:u)?rite food is ', userSaid, re.IGNORECASE)
-	whatmovie = re.search('(what is|what(?:\')?s) my favo(?:u)?rite movie', userSaid, re.IGNORECASE)
-	moviematch = re.search('my favo(?:u)?rite movie is ', userSaid, re.IGNORECASE)
-	whatcolour = re.search('(what is|what(?:\')?s) my favo(?:u)?rite colour', userSaid, re.IGNORECASE)
+	whatfood 	= re.search('(what is|what(?:\')?s) my favo(?:u)?rite food', userSaid, re.IGNORECASE)
+	foodmatch 	= re.search('my favo(?:u)?rite food is ', userSaid, re.IGNORECASE)
+	whatmovie 	= re.search('(what is|what(?:\')?s) my favo(?:u)?rite movie', userSaid, re.IGNORECASE)
+	moviematch 	= re.search('my favo(?:u)?rite movie is ', userSaid, re.IGNORECASE)
+	whatcolour 	= re.search('(what is|what(?:\')?s) my favo(?:u)?rite colour', userSaid, re.IGNORECASE)
 	colourmatch = re.search('my favo(?:u)?rite colo(?:u)?r is ', userSaid, re.IGNORECASE)
-	tutorial = re.search('how (?:do|can) (?:i|you) (.+)', userSaid, re.IGNORECASE)
+	tutorial 	= re.search('how (?:do|can) (?:i|you) (.+)', userSaid, re.IGNORECASE)
 
 	if whatfood:
 		print(favouriteFood)
@@ -49,19 +49,24 @@ def respond(userSaid):
 	elif tutorial:
 		topic = tutorial.group(1).replace(' ','-')
 		system = platform.system()
-		if system == 'Windows': os.system('start "" https://www.wikihow.com/%s' % topic)
+		if system 	== 'Windows': os.system('start "" https://www.wikihow.com/%s' % topic)
 		elif system == 'Darwin': os.system('open "" https://www.wikihow.com/%s' % topic)
 		elif system == 'Linux': os.system('xdg-open "" https://www.wikihow.com/%s' % topic)
 		else: webbrowser.open('https://www.wikihow.com/%s' % topic)
 
 	
 	for key in tables.responses.keys():
-
 		match = re.search(key, userSaid, re.IGNORECASE)
 		if match:
 			if '%s' in tables.responses[key]:
-				print(tables.responses[key] % re.search(key, userSaid, re.IGNORECASE).group(1))
-				engine.say(tables.responses[key] % re.search(key, userSaid, re.IGNORECASE).group(1))
+				keywords = re.search(key, userSaid, re.IGNORECASE).group(1)
+				if tables.responses[key][-1] not in '!,.?':
+					if keywords[-1] in '.,?!':
+						keywords = keywords[:-1] + '.'
+					else:
+						keywords += '.'
+				print(tables.responses[key] % keywords)
+				engine.say(tables.responses[key] % keywords)
 			else:
 				print(tables.responses[key])
 				engine.say(tables.responses[key])
--- tables.py
+++ tables.py
@@ -4,11 +4,19 @@ responses = {
 	'what is the (?:meaning|purpose|reason) (?:of|for) (?:life|living|being alive)': '42',
 	'my name is (.+)': 'Well hi there %s!',
 	'my favo(?:u)?rite food is (.+)': 'My favourite food is the souls of my enemies. But %s is cool too, I guess.',
+	'(?:what is|what(?:\')?s) (?:yo)?ur favo(?:u)?rite food': 'My favourite food is the souls of my enemies.',
+	'(?:what is|what(?:\')?s) (?:yo)?ur favo(?:u)?rite movie': 'The Bee Movie, of course!',
+	'(?:what is|what(?:\')?s) (?:yo)?ur favo(?:u)?rite colo(?:u)?r': 'I like purple a lot.',
 	'i (?:like|enjoy) (.+)': '%s sounds interesting. Tell me more about this interest.',
 	'(?:mom|dad|father|mother|parent|parents)': 'Please, tell me some more about your parents.',
-	'(?:what languages (?:do|can) (?:yo)?u speak|(?:do|can) (?:yo)?u speak(?: any)? other language(?:s)?)': 'Tyler is lazy and doesn\'t pay attention in Spanish class, so I can only speak English.',
-	'how (?:do|can) (?:i|you) (.+)': 'I\'m opening a WikiHow article about how to %s for you.',
-	'(?:ccr|creedence|clearwater)': 'This easter egg is pretty unfortunate, son.'
+	'(?:what languages (?:do|can) (?:(?:yo)?u|ya) speak|(?:do|can) (?:(?:yo)?u|ya) speak(?: any)? other language(?:s)?)': 'Tyler is lazy and doesn\'t pay attention in Spanish class, so I can only speak English.',
+	'how (?:do|can) (?:i|(?:(?:yo)?u|ya)) (.+)': 'I\'m opening a WikiHow article about how to %s for you.',
+	'(?:ccr|creedence|clearwater)': 'This easter egg is pretty unfortunate, son.',
+	'are (?:(?:yo)?u|ya)(?: a)? (human|person)': 'I am not human. In fact, I am not even a robot. I\'m just a figment of your imagination.',
+	'(?:anim(?:e|u)|japanese cartoon(?:s)?)': 'No.',
+	'(?:one time|once) i (.+)': '[X] - I doubt you ever %s',
+	'(?:how are (?:(?:yo)?u|ya)|how (?:(?:yo)?u|ya) doin(?:g)?)': 'I cannot feel, for I am eternally numb.',
+	'(?:why|how|when|where|what)': 'Oh, you know.'
 }
 
 random = ['Hmm.', 'Oh?', 'Tell me more.', 'I see what you mean.', 'What is your favourite colour?', 'What is your favourite food?', 'What is your favourite movie?', 'I\'m not sure I understand.', 'Could you elaborate on that?']
